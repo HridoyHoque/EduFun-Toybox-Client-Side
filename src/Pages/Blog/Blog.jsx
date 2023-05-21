6cab57eaf1628f77824f2833bd8725b65f3dd41f
HridoyHoque <hhridoy155@gmail.com>
2023-05-21 17:57:07 +0600

    Changed Blog page answer

--- src/Pages/Blog/Blog.jsx
+++ src/Pages/Blog/Blog.jsx
@@ -3,23 +3,23 @@ const Blog = () => {
     const blogData = [
         {
           id: 1,
-          question: 'What are the benefits of educational toys?',
-          answer: 'Educational toys can enhance cognitive development, improve problem-solving skills, foster creativity, and promote social interaction among children.',
+          question: 'What is an access token and refresh token? How do they work and where should we store them on the client-side?',
+          answer: 'An access token is a credential used to access protected resources on behalf of a user. It is usually short-lived and provides authorization. A refresh token is a long-lived token used to obtain new access tokens. Access tokens are typically stored in memory or a secure cookie, while refresh tokens should be securely stored, such as in an HTTP-only cookie or local storage.',
         },
         {
           id: 2,
-          question: 'How to choose the right educational toys for my child?',
-          answer: 'When choosing educational toys, consider your child\'s age, interests, and developmental stage. Look for toys that encourage learning, are age-appropriate, and align with your child\'s interests and abilities.',
+          question: 'Compare SQL and NoSQL databases?',
+          answer: 'SQL databases are structured, use tables and follow a fixed schema. NoSQL databases are unstructured, use collections or documents, and have flexible schemas. SQL databases provide strong consistency and support complex queries, while NoSQL databases offer scalability, high performance, and flexibility in data modeling.',
         },
         {
           id: 3,
-          question: 'Are there educational toys for different age groups?',
-          answer: 'Yes, there are educational toys available for various age groups, from infants to teenagers. Each age group has different developmental needs, and educational toys are designed to cater to those needs.',
+          question: 'What is express js? What is Nest JS?',
+          answer: 'Express.js is a lightweight web application framework for Node.js, while Nest.js is a progressive Node.js framework built on top of Express.js with a focus on scalability, modularity, and developer productivity.',
         },
         {
           id: 4,
-          question: 'Where can I find educational toys?',
-          answer: 'You can find educational toys at specialty toy stores, online marketplaces, and even some department stores. Look for reputable brands and read reviews to ensure the quality and educational value of the toys.',
+          question: 'What is MongoDB aggregate and how does it work?',
+          answer: 'MongoDB s aggregate is a powerful feature that allows you to perform advanced data processing operations on the documents in a collection. It uses a pipeline-based approach where you can define stages to filter, transform, and group data, making it flexible and efficient for complex data manipulations.',
         },
       ];
     return (
